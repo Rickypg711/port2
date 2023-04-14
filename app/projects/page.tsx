5b4c7ffc7713ec9ddc2eea36682efd0039dfdffe
Rickypg711 <77697094+Rickypg711@users.noreply.github.com>
2023-04-13 20:54:01 -0700

    addedprojects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import clima from '../../public/images/Image 3-8-23 at 5.48 PM.jpg'
+import quote from '../../public/images/Image 4-13-23 at 8.36 PM.jpg'
+import roket from '../../public/images/Image 4-13-23 at 8.44 PM.jpg'
 
 import { FaGithub } from "react-icons/fa";
 
@@ -55,8 +57,11 @@ export default function Projects() {
               rel="noopener noreferrer"
             >
               <div className="bg-teal-200 hover:bg-violet-200 text-white font-bold py-2 px-4 rounded-full transition-all flex items-center">
-                <FaGithub size={20} className="mr-2" />
+             
+              <FaGithub size={20} className="mr-2" />
                 <span>View on GitHub</span>
+                &nbsp;
+                <h2>climaap</h2>
               </div>
             </Link>
           </div>
@@ -64,26 +69,29 @@ export default function Projects() {
 
         <div className="w-full md:w-1/2 lg:w-1/3 m-5">
           <Link
-            href="https://clima-rust.vercel.app/"
+            href="https://roket.vercel.app/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              src={clima}
-              alt="Preview of Clima project"
+              src={roket}
+              alt="Preview of wikiRoket project"
               // object-fit="fill"
               className="w-full h-auto rounded-lg shadow-lg object-cover	"
             />
+            
           </Link>
           <div className="flex items-center justify-center mt-5">
             <Link
-              href="https://github.com/Rickypg711/clima"
+              href="https://github.com/Rickypg711/roket"
               target="_blank"
               rel="noopener noreferrer"
             >
               <div className="bg-teal-200 hover:bg-violet-200 text-white font-bold py-2 px-4 rounded-full transition-all flex items-center">
                 <FaGithub size={20} className="mr-2" />
                 <span>View on GitHub</span>
+                &nbsp;
+                <h2>Roket</h2>
               </div>
             </Link>
           </div>
@@ -92,20 +100,20 @@ export default function Projects() {
         {/* third */}
         <div className="w-full md:w-1/2 lg:w-1/3 m-5">
           <Link
-            href="https://clima-rust.vercel.app/"
+            href="https://quote-api-rho.vercel.app/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              src={clima}
-              alt="Preview of Clima project"
-              // object-fit='fill'
+              src={quote}
+              alt="Preview of quote api project"
+             
               className="w-full h-auto rounded-lg shadow-lg"
             />
           </Link>
           <div className="flex items-center justify-center mt-5">
             <Link
-              href="https://github.com/Rickypg711/clima"
+              href="https://github.com/Rickypg711/quote-api"
               target="_blank"
               rel="noopener noreferrer"
             >
