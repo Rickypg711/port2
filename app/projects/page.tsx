d9b3f84ce3d9020d4a7e92a73b2a78e27dd29cac
Rickypg711 <77697094+Rickypg711@users.noreply.github.com>
2023-04-13 18:49:39 -0700

    addingprojects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import clima from '../../public/images/Image 3-8-23 at 5.48 PM.jpg'
 
 import { FaGithub } from "react-icons/fa";
 
@@ -33,73 +34,6 @@ export default function Projects() {
   return (
     <div>
       <Nav />
-      {/* hamburger */}
-
-      {/* <div className="flex mt-3 items-center">
-        <div className="ml-3 mr-7">
-          <Link
-            href="/"
-            className={`${playFairDisplay.className}  text-teal-200 text-xl hover:line-through decoration-violet-200 transition-all`}
-          >
-            RICARDO PAREDES
-          </Link>
-        </div>
-        <div className="nav-line is-pink"></div>
-        <div className="flex-grow h-px bg-violet-200 mx-auto"></div>
-
-        <div className="nav-left-item nav-copyright">
-        <div className="nav-left-item nav-links  ml-7 mr-4 text-teal-200 text-xl">
-          <button
-            className="text-2xl text-teal-200 hover:text-violet-200 focus:outline-none"
-            onClick={toggleMenu}
-          >
-            <FaBars />
-          </button>
-          <div
-            className={`${
-              isOpen ? "block" : "hidden"
-            } absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20`}
-          >
-            <Link
-              href="/about"
-              className="block px-4 py-2 text-gray-800 hover:bg-violet-200 hover:text-white"
-            >
-              About
-            </Link>
-            <Link
-              href="/projects"
-              className="block px-4 py-2 text-gray-800 hover:bg-violet-200 hover:text-white"
-            >
-              Projects
-            </Link>
-            <Link
-              href="/blog"
-              className="block px-4 py-2 text-gray-800 hover:bg-violet-200 hover:text-white"
-            >
-              Blog
-            </Link>
-          </div>
-        </div>
-        </div>
-      </div> */}
-
-      {/* hamburger */}
-      {/* <div className="flex mt-3 items-center">
-        <div className="ml-3 mr-7">
-          <Link
-            href="/"
-            className={`${playFairDisplay.className}  text-teal-200 text-xl hover:line-through decoration-violet-200 transition-all`}
-          >
-            RICARDO PAREDES
-          </Link>
-        </div>
-        <div className="nav-line is-pink"></div>
-        <div className="flex-grow h-px bg-violet-200 mx-auto"></div>
-        <div className="nav-left-item nav-copyright">
-          <div className="ml-7 mr-3 text-teal-200 text-xl">©/2023</div>
-        </div>
-      </div> */}
-      {/*  */}
       <div className="my-10 flex flex-wrap md:flex-no-wrap justify-center">
         <div className="w-full md:w-1/2 lg:w-1/3 m-5">
           <Link
@@ -107,12 +41,12 @@ export default function Projects() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            {/* <Image
+            <Image
               src={clima}
               alt="Preview of Clima project"
               // object-fill
               className="w-full h-auto rounded-lg shadow-lg object-cover	"
-            /> */}
+            />
           </Link>
           <div className="flex items-center justify-center mt-5">
             <Link
@@ -134,12 +68,12 @@ export default function Projects() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            {/* <Image
+            <Image
               src={clima}
               alt="Preview of Clima project"
               // object-fit="fill"
               className="w-full h-auto rounded-lg shadow-lg object-cover	"
-            /> */}
+            />
           </Link>
           <div className="flex items-center justify-center mt-5">
             <Link
@@ -162,12 +96,12 @@ export default function Projects() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            {/* <Image
+            <Image
               src={clima}
               alt="Preview of Clima project"
               // object-fit='fill'
               className="w-full h-auto rounded-lg shadow-lg"
-            /> */}
+            />
           </Link>
           <div className="flex items-center justify-center mt-5">
             <Link
